d3d15c57beef7d37145acaad5d9fc78472c9ecdd
Raweewat Somboonyarithi <bookky_over_@hotmail.com>
2026-05-19 10:20:48 +0700

    New update 10.20

--- js/database.js
+++ js/database.js
@@ -1,12 +1,21 @@
 function initContactForm() {
-
   const form = document.getElementById("myContactForm");
 
   if (!form) return;
 
-  form.addEventListener("submit", function (e) {
+  const submitBtn = document.getElementById("submitBtn");
 
+  form.addEventListener("submit", function (e) {
     e.preventDefault();
+
+    // กันกดรัว
+    submitBtn.disabled = true;
+
+    // เปลี่ยนข้อความ
+    submitBtn.innerHTML = `
+  <span class="spinner"></span>
+  Sending...
+`;
 
     const formData = new FormData(this);
 
@@ -15,22 +24,25 @@ function initContactForm() {
       {
         method: "POST",
         body: formData,
-      }
+      },
     )
       .then((res) => res.json())
       .then((data) => {
-
         alert("ขอบคุณที่ติดต่อเรา! เราจะรีบติดต่อกลับโดยเร็วที่สุด");
 
         form.reset();
-
       })
       .catch((error) => {
-
         console.error("Error!", error.message);
 
+        alert("เกิดข้อผิดพลาด กรุณาลองใหม่อีกครั้ง");
+      })
+      .finally(() => {
+        // เปิดปุ่มกลับ
+        submitBtn.disabled = false;
+
+        // คืนข้อความเดิม
+        submitBtn.innerHTML = "Submit";
       });
-
   });
-
 }
